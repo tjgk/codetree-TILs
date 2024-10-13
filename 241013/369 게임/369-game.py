9e4f67da6dde013d7aa466335a74386971a8065a
tjgk <70050285+tjgk@users.noreply.github.com>
2024-10-13 16:02:08 +0900

    Title: 369 게임, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 241013/369 게임/369-game.py
+++ 241013/369 게임/369-game.py
@@ -1,3 +1,3 @@
 n=int(input())
 for i in range(1,n+1):
-    print(0 if i%3==0 or "3" in str(i) else i,end=" ")
+    print(0 if i%3==0 or "3" in str(i) or "6" in str(i) or "9" in str(i) else i,end=" ")
