fa0cd70a1c278cc8832e4e2accb4f1cea31e1bdb
이서하 <70050285+tjgk@users.noreply.github.com>
2025-10-25 22:41:35 +0900

    Title: 19단 출력, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251025/19단 출력/nineteen-times-table.py
@@ -0,0 +1,8 @@
+for i in range(1,20):
+    for j in range(1,20):
+        print(f"{i} * {j} = {i*j}",end="")
+        if j%2==1 and j!=19:
+            print(" / ",end="")
+        if j%2==0:
+            print()
+    print()
