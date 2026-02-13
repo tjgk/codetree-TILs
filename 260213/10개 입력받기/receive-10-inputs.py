92e47cb4cf11365c1316f2e5f5a8d92a4b6bb284
이서하 <70050285+tjgk@users.noreply.github.com>
2026-02-13 15:44:47 +0900

    Title: 10개 입력받기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260213/10개 입력받기/receive-10-inputs.py
+++ 260213/10개 입력받기/receive-10-inputs.py
@@ -1,8 +1,11 @@
 a=list(map(int,input().split()))
 s,t=0,0
+tf=0
 for i in a:
     if i!=0:
         s+=i
         t+=1
     else:
         print(f"{s} {s/t:.1f}")
+        tf=1
+if tf==0: print(f"{s} {s/10:.1f}")
