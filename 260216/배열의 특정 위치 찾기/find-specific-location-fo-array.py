d8490c482d5294291181940368ab46185b671452
이서하 <70050285+tjgk@users.noreply.github.com>
2026-02-16 13:05:50 +0900

    Title: 배열의 특정 위치 찾기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260216/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 260216/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -1,8 +1,9 @@
-a,b,c=0,0,0
+a,b,c,d=0,0,0,0
 for i in list(map(int,input().split())):
-    if i%2==0:
+    d+=1
+    if d%2==0:
         a+=i
-    if i%3==0:
+    if d%3==0:
         b+=i
         c+=1
 print(f"{a} {b/c:.1f}")
